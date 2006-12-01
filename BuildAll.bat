e810cb3950ec136ff6c60465f6d0d4506693885e
Kevin Owen <kevin.owen@neumont.edu>
2006-12-01 20:16:48 +0000

    Added Version.cs and Version.wxi to svn:ignore for root folder. Fixed BuildAll.bat to work on Windows Vista and to use MSBuild rather than devenv.exe. refs #6
    SVN Revision: 42

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,8 +1,7 @@
-@setlocal
-@set BuildType=Debug
-@FOR /F "usebackq skip=3 tokens=2*" %%A IN (`REG QUERY "HKLM\SOFTWARE\Microsoft\VisualStudio\8.0" /v "InstallDir"`) DO call set LaunchDevenv=%%~dpsBdevenv
-%launchdevenv% "%~dp0CodeGen.sln" /Rebuild %BuildType%
-call "%~dp0CodeGenCustomTool\Install.bat"
-%launchdevenv% "%~dp0PLiXReflector.sln" /Rebuild %BuildType%
-call "%~dp0Reflector\Install.bat"
-%launchdevenv% "%~dp0Setup/Setup.sln" /Rebuild Setup
+@ECHO OFF
+SETLOCAL
+IF NOT DEFINED FrameworkSDKDir (CALL "%VS80COMNTOOLS%\vsvars32.bat")
+SET RootDir=%~dp0.
+MSBuild.exe /nologo "%RootDir%\CodeGen.sln" %*
+MSBuild.exe /nologo "%RootDir%\PLiXReflector.sln" %*
+MSBuild.exe /nologo "%RootDir%\Setup\Setup.sln" %*
