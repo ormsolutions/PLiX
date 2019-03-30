7853b719c9ea114d7c00cb57c1ca1d2b81427ddf
Matthew Curland <mcurland@live.com>
2019-03-30 10:59:44 -0600

    Reenable pre-VS2017 Builds
    Explicit CTC compile to CTO used in previous versions requires
    different .resx file.

--- BuildAll.bat
+++ BuildAll.bat
@@ -2,6 +2,10 @@
 SETLOCAL
 IF NOT DEFINED FrameworkSDKDir (CALL "%VS80COMNTOOLS%\vsvars32.bat")
 SET RootDir=%~dp0.
-MSBuild.exe /nologo "%RootDir%\CodeGen.sln" %*
-MSBuild.exe /nologo "%RootDir%\PLiXReflector.sln" %*
-MSBuild.exe /nologo "%RootDir%\Setup\Setup.sln" %*
+IF "%TargetVisualStudioNumericVersion%"=="15.0" (
+	MSBuild.exe /nologo "%RootDir%\CodeGen.sln" %*
+) ELSE (
+	MSBuild.exe /nologo "%RootDir%\CodeGen.sln" %* /toolsversion:2.0
+	MSBuild.exe /nologo "%RootDir%\PLiXReflector.sln" %*
+	MSBuild.exe /nologo "%RootDir%\Setup\Setup.sln" %*
+)
