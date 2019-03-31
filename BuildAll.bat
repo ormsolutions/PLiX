0381781c36008bb8658cd506efc623caa5b3f81e
Matthew Curland <mcurland@live.com>
2019-03-31 01:31:00 -0600

    Add VSIX Generator to VS2017 build script

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,10 +1,11 @@
 @ECHO OFF
 SETLOCAL
-IF NOT DEFINED FrameworkSDKDir (CALL "%VS80COMNTOOLS%\vsvars32.bat")
 SET RootDir=%~dp0.
 IF "%TargetVisualStudioNumericVersion%"=="15.0" (
-	MSBuild.exe /nologo "%RootDir%\CodeGen.sln" %*
+	MSBuild.exe /nologo "%RootDir%\CodeGen.VS2017.sln" %*
+	MSBuild.exe /nologo "%RootDir%\VSIXInstall\VSIXOnly\PLiXVSIX.VS2017.sln" %*
 ) ELSE (
+	IF NOT DEFINED FrameworkSDKDir (CALL "%VS80COMNTOOLS%\vsvars32.bat")
 	MSBuild.exe /nologo "%RootDir%\CodeGen.sln" %* /toolsversion:2.0
 	MSBuild.exe /nologo "%RootDir%\PLiXReflector.sln" %*
 	MSBuild.exe /nologo "%RootDir%\Setup\Setup.sln" %*
