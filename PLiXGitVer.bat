ef0e799ade913f371585f64333c31d4a9fe7eb7c
Matthew Curland <mcurland@live.com>
2021-03-23 17:35:34 -0600

    Git versioning and per-user install
    Make versioning on all files git-based.
    
    With VSIXPerUser=1 environment variable set, create a per-user
    installation. This does not install schema files with VS, but it
    does adjust the schema catalog file so that this can be opened
    directly in Visual Studio, which causes the XML editor to recognize
    the schema files listed in the catalog.

--- PLiXGitVer.bat
+++ PLiXGitVer.bat
@@ -1,13 +1,37 @@
 @ECHO OFF
 SETLOCAL
-FOR /F "usebackq delims=v.-g tokens=1,2,3,4" %%i in (`git -C "%1" describe --first-parent --long %2`) DO (
+FOR /F "usebackq delims=v.-g tokens=1,2,3,4,5" %%i in (`git -C "%2" describe --first-parent --long %3`) DO (
 SET GitMajor=%%i
 SET GitMinor=%%j
 SET GitSub=%%k
 SET GitUpdate=%%l
+SET GitHash=%%m
 )
 SET PadSub=00%GitSub%
 REM Put a 3 in front of the tagged update number. This distinguished the git tag
 REM based versioning system (and makes the numbers larger than) previous versioning
 REM schemes were year/month based.
+IF "%1"=="all" (
 @ECHO %GitMajor%.%GitMinor%.3%PadSub:~-3%.%GitUpdate%
+)
+IF "%1"=="" (
+@ECHO %GitMajor%.%GitMinor%.3%PadSub:~-3%.%GitUpdate%
+)
+IF "%1"=="full" (
+@ECHO %GitMajor%.%GitMinor%.3%PadSub:~-3%.%GitUpdate%-g%GitHash%
+)
+IF "%1"=="major" (
+@ECHO %GitMajor%
+)
+IF "%1"=="minor" (
+@ECHO %GitMinor%
+)
+IF "%1"=="build" (
+@ECHO 3%PadSub:~-3%
+)
+IF "%1"=="revision" (
+@ECHO %GitUpdate%
+)
+IF "%1"=="hash" (
+@ECHO %GitHash%
+)
